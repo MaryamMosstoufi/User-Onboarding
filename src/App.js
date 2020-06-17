0c8ad3035f55b239f88f6eb6c3e8e3619b673d88
MaryamMosstoufi <maryam@zooxoos.com>
2020-06-17 18:06:40 -0400

    Form with Post and Display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,101 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import formSchema from '../validation/formSchema'
+import * as Yup from 'yup';
+import axios from 'axios';
 
 import Form from './components/Form.js';
+import Users from './components/Users.js';
 
+const initialUsers = [
+  {
+    name: 'Maryam',
+    email: 'maryam@maryam.com',
+    password: '123456789',
+    terms: true,
+  }
+];
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  terms: false
+};
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+  terms: '',
+};
+const initialDisabled = false;
 
 function App() {
+  const [users, setUsers] = useState(initialUsers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+
+  const postNewUser = newUser => {
+    axios.post('https://reqres.in/api/users', newUser)
+      .then(response => {
+        setUsers([...users, response.data])
+      })
+      .catch(error => {
+        debugger
+      })
+      .finally(() => {
+        setFormValues(initialFormValues)
+      })
+  }
+
+  const onInputChange = evt => {
+    const name = evt.target.name
+    const value = evt.target.value
+
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    })
+  };
+  const onCheckboxChange = evt => {
+    const { name, checked } = evt.target
+    setFormValues({
+      ...formValues,
+      [name]: checked,
+    })
+  };
+  const onSubmit = evt => {
+    evt.preventDefault()
+
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      terms: formValues.terms,
+    };
+    postNewUser(newUser)
+  };
+
+
   return (
     <div className="App">
       <h1>User Onboarding</h1>
-      <Form />
+      <Form
+        values={formValues}
+        onInputChange={onInputChange}
+        onCheckboxChange={onCheckboxChange}
+        onSubmit={onSubmit}
+        disabled={disabled}
+        errors={formErrors}
+      />
+      {
+        users.map(user => {
+          return (
+            <Users key={user.password} user={user} />
+          )
+        })
+      }
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,53 @@
 import React from 'react';
 
 
-export default function Form() {
+export default function Form(props) {
+    const {
+        values,
+        onSubmit,
+        onInputChange,
+        onCheckboxChange,
+        disabled,
+        errors,
+    } = props
 
     return (
         <div>
-            form
-        </div>
+            <form onSubmit={onSubmit}>
+                <label>Name&nbsp;
+                    <input
+                        value={values.name}
+                        onChange={onInputChange}
+                        name='name'
+                        type='text'
+                    />
+                </label>
+                <label>Email&nbsp;
+                    <input
+                        value={values.email}
+                        onChange={onInputChange}
+                        name='email'
+                        type='email'
+                    />
+                </label>
+                <label>Password&nbsp;
+                    <input
+                        value={values.password}
+                        onChange={onInputChange}
+                        name='password'
+                        type='password'
+                    />
+                </label>
+                <label>Terms of Service
+                    <input
+                        checked={values.terms}
+                        onChange={onCheckboxChange}
+                        name='terms'
+                        type='checkbox'
+                    />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div >
     )
 };
